4b7329e787226ea1701693ca7f8ce3f080d24808
Eitaro Fukamachi <e.arrows@gmail.com>
2015-08-13 17:00:17 +0900

    Add tests for compose-frame.

--- t/compose.lisp
+++ t/compose.lisp
@@ -2,13 +2,14 @@
 (defpackage fast-websocket-test.compose
   (:use :cl
         :fast-websocket.compose
+        :fast-websocket-test.util
         :trivial-utf-8
         :prove)
   (:import-from :fast-websocket
                 #:error-code))
 (in-package :fast-websocket-test.compose)
 
-(plan nil)
+(plan 4)
 
 (subtest "string"
   (is (compose-frame "hi") #(129 2 104 105) :test #'equalp)
@@ -21,8 +22,30 @@
   (is (compose-frame (string-to-utf-8-bytes "hi") :type :text) #(129 2 104 105) :test #'equalp))
 
 (subtest "close"
-  (is (compose-frame "hi" :type :close :code (error-code :normal-closure))
-      #(136 4 3 232 104 105)
+  (is (compose-frame "bye" :type :close :code (error-code :normal-closure))
+      #(136 5 3 232 98 121 101)
       :test #'equalp))
+
+(defun constant-random-mask-keys ()
+  (bv 186 43 99 37))
+
+(subtest "masking"
+  (let ((original #'fast-websocket.compose::random-mask-keys))
+    (setf (fdefinition 'fast-websocket.compose::random-mask-keys)
+          #'constant-random-mask-keys)
+
+    (is (compose-frame "hi" :masking t)
+        #(129 130 186 43 99 37 210 66)
+        :test #'equalp)
+
+    (is (compose-frame "bye"
+                       :type :close
+                       :code (error-code :normal-closure)
+                       :masking t)
+        #(136 133 186 43 99 37 185 195 1 92 223)
+        :test #'equalp)
+
+    (setf (fdefinition 'fast-websocket.compose::random-mask-keys)
+          original)))
 
 (finalize)
