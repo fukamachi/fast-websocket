49aa11ab19c66485281e6a80b02bea66aa379a68
Eitaro Fukamachi <e.arrows@gmail.com>
2015-08-13 22:10:57 +0900

    Delete needless code.

--- t/util.lisp
+++ t/util.lisp
@@ -6,8 +6,3 @@
 
 (defun bv (&rest args)
   (make-array (length args) :element-type '(unsigned-byte 8) :initial-contents args))
-
-(defun random-mask-keys ()
-  (let ((keys (make-array 4 :element-type '(unsigned-byte 8))))
-    (dotimes (i 4 keys)
-      (setf (aref keys i) (random 256)))))
